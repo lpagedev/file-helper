05688a458ec57e832c556eb669c1221aafbbaae5
lpagedev <levipage@gmail.com>
2022-01-05 10:05:42 -0800

    Update FileHelper.php
    Add option to put column headers in CSV

--- src/FileHelper.php
+++ src/FileHelper.php
@@ -30,8 +30,7 @@ class FileHelper
             $file = fopen($pFilePath, "w");
             $res = fwrite($file, $pContents);
             fclose($file);
-            if (file_exists($pFilePath))
-                return $res;
+            if (file_exists($pFilePath)) return $res;
         }
         return false;
     }
@@ -48,12 +47,10 @@ class FileHelper
 
         $handle = fopen($pFilePath, 'r');
         while (($data = fgetcsv($handle)) !== FALSE) {
-            if (!is_null($data))
-                $rows[] = $data;
+            if (!is_null($data)) $rows[] = $data;
         }
         ini_set('auto_detect_line_endings', $auto_detect_line_endings);
-        if (count($rows) > 0)
-            return $rows;
+        if (count($rows) > 0) return $rows;
         return null;
     }
 
@@ -62,19 +59,25 @@ class FileHelper
      * @param array $pRows
      * @return bool
      */
-    public static function CSVWrite(string $pFilePath, array $pRows): bool
+    public static function CSVWrite(string $pFilePath, array $pRows, bool $pAddHeader = true, string $pSeparator = ',', string $pEnclosure = '"', string $pEscape = '\\'): bool
     {
         if (!is_dir($pFilePath)) {
             $file = fopen($pFilePath, "w");
             $length = 0;
             foreach ($pRows as $row) {
-                $res = fputcsv($file, $row);
-                if ($res)
-                    $length += $res;
+                if ($pAddHeader && $length == 0) {
+                    $header = [];
+                    foreach ($row as $name => $value) {
+                        $header[] = $name;
+                    }
+                    $res = fputcsv($file, $header, $pSeparator, $pEnclosure, $pEscape);
+                    if ($res) $length += $res;
+                }
+                $res = fputcsv($file, $row, $pSeparator, $pEnclosure, $pEscape);
+                if ($res) $length += $res;
             }
             fclose($file);
-            if (file_exists($pFilePath))
-                return $length;
+            if (file_exists($pFilePath)) return $length;
         }
         return false;
     }
